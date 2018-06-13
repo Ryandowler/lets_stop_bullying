581724184f52c6de0d934499021ba60bdd1b1773
Alex Keaveney <ak12349@hotmail.com>
2018-06-13 18:49:22 +0100

    Database connection

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const path = require('path');
 const generatePassword = require('password-generator');
+const MongoClient = require('mongodb').MongoClient;
+let db;
 
 const app = express();
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
+
+
+
+// MongoClient.connect('mongodb://127.0.0.1:27017', (err, database) => {
+MongoClient.connect('mongodb://test:letsstop1@ds259410.mlab.com:59410/lets-stop-bullying', (err, database) => {
+
+    //if there is an error log it
+    if (err) { console.log(err); }
+    //if there are no errors
+    else {
+        //set db object to 'lets-stop-bullying' database
+        db = database.db('lets-stop-bullying');
+
+        db.collection("content").find({}).toArray(function(err, result) {
+        if (err) throw err;
+            console.log(result);
+        });
+    }
+
+    //listen for requests to db on port 8000
+    app.listen(8000, () => {
+        console.log('Database connected');
+    });
+});
 
 // Put all API endpoints under '/api'
 app.get('/api/passwords', (req, res) => {
